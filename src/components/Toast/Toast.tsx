e9c945ac510335d6724337f4ba941c8d8bcac585
Andrew Fletcher <andrewf3009@gmail.com>
2022-03-24 01:16:05 +0000

    Removes toast from the toastlist when x button is clicked

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -4,12 +4,15 @@ import "./Toast.css";
 export interface ToastProps {
 	type: "success" | "error" | "warning" | "info";
   message: string;
+	onClose: () => void;
 }
 
 function Toast(props: ToastProps) {
   return (
 		<div className={`notification toast ${props.type}`}>
-			<button>x</button>
+			<button onClick={() => props.onClose()}>
+				x
+			</button>
 
 			<div className="icon-container">
 				<i className="fas fa-check"></i>
